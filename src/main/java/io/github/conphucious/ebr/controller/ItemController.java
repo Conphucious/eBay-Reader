70fe95b8154fab2bf0ed6d2706b28c6c27ba548d
root <root@LAPTOP-M8VOMFDC.localdomain>
2021-04-04 23:28:15 -0500

    added to identifiers

--- src/main/java/io/github/conphucious/ebr/controller/ItemController.java
+++ src/main/java/io/github/conphucious/ebr/controller/ItemController.java
@@ -1,15 +1,15 @@
 package io.github.conphucious.ebr.controller;
 
-import io.github.conphucious.ebr.model.Identifiers;
+import io.github.conphucious.ebr.model.Identifier;
 import io.github.conphucious.ebr.model.Item;
 import io.github.conphucious.ebr.model.ItemStatus;
 import io.github.conphucious.ebr.model.ItemType;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class ItemController {
 
@@ -21,11 +21,11 @@ public class ItemController {
             ItemStatus itemStatus = getStatus(document);
 
             if (itemStatus == ItemStatus.ENDED) {
-                System.out.println("ENDED");
-                // item type stays null
+                System.out.println("ENDED"); // item type stays null
                 return null;
+
+
             } else if (itemStatus == ItemStatus.IN_PROGRESS) {
-                ItemType itemType = getType(document);
             }
 
             ItemType itemType = getType(document);
@@ -42,7 +42,7 @@ public class ItemController {
     }
 
     private ItemStatus getStatus(Document document) {
-        Element statusElement = document.getElementById(Identifiers.ITEM_STATUS_IDENTIFIER);
+        Element statusElement = document.getElementById(Identifier.ITEM_STATUS);
         return statusElement == null ? ItemStatus.IN_PROGRESS : ItemStatus.ENDED;
     }
 
@@ -50,10 +50,10 @@ public class ItemController {
         ItemType itemType = null;
 
         // Check if this is a BID
-        Element priceElement = document.getElementById(Identifiers.ITEM_TYPE_BID_IDENTIFIER);
+        Element priceElement = document.getElementById(Identifier.ITEM_TYPE_BID_ID);
         if (priceElement == null) {
-            // Check if this is a BEST_OFFER or BUY
-            Element bestOfferElement = document.getElementById(Identifiers.ITEM_TYPE_BEST_OFFER_IDENTIFIER);
+            // Check if this is a BEST_OFFER or BUY -> Assume if not BEST_OFFER then it's BUY.
+            Element bestOfferElement = document.getElementById(Identifier.ITEM_TYPE_BEST_OFFER_ID);
             itemType = bestOfferElement == null ? ItemType.BUY : ItemType.BEST_OFFER;
         } else {
             itemType = ItemType.BID;
@@ -66,13 +66,18 @@ public class ItemController {
         System.out.println(itemType);
         Element priceElement = null;
         if (itemType == ItemType.BEST_OFFER) {
-            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BEST_OFFER_IDENTIFIER);
+            priceElement = document.getElementById(Identifier.ITEM_PRICE_BEST_OFFER_ID);
         } else if (itemType == ItemType.BUY) {
-            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BUY_IDENTIFIER);
+            priceElement = document.getElementById(Identifier.ITEM_PRICE_BUY_ID);
         } else if (itemType == ItemType.BID) {
-            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BID_IDENTIFIER);
+            priceElement = document.getElementById(Identifier.ITEM_PRICE_BID_ID);
         }
         return priceElement.text();
+    }
+
+    private Date[] getDates(Document document) {
+
+        return null;
     }
 
 
---
+++ src/main/java/io/github/conphucious/ebr/model/Identifier.java
@@ -0,0 +1,26 @@
+package io.github.conphucious.ebr.model;
+
+public class Identifier {
+
+    // ITEM STATUS HTML ID
+    public static final String ITEM_STATUS = "w1-5-_msg";
+
+    // ITEM PRICE HTML ID
+    public static final String ITEM_PRICE_BID_ID = "prcIsum_bidPrice";
+    public static final String ITEM_PRICE_BUY_ID = "prcIsum";
+    public static final String ITEM_PRICE_BEST_OFFER_ID = "mm-saleDscPrc";
+
+    // ITEM TYPES HTML ID
+    public static final String ITEM_TYPE_BID_ID = "prcIsum_bidPrice";
+    public static final String ITEM_TYPE_BEST_OFFER_ID = "boBtn_btn";
+
+
+    // SELECT FROM JSOUP DOCUMENT
+    public static final String SELECT_ELEMENTS_TITLE = "boBtn_btn";
+    public static final String SELECT_ELEMENTS_TIME_LEFT = "span[id=\"vi-cdown_timeLeft\"]";
+    public static final String SELECT_ELEMENTS_DATE_END = "span[class=\"vi-tm-left\"]";
+
+    // REGEXP PATTERNS
+    public static final String REGEX_ITEM_DATE = "(?<=timems=\")(.*)(?=\")";
+
+}
