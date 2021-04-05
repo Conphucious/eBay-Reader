480c71f70ec2a55c1eba2cce19919499eb9c0368
root <root@LAPTOP-M8VOMFDC.localdomain>
2021-04-04 23:16:58 -0500

    added type and pricing

--- src/main/java/io/github/conphucious/ebr/controller/ItemController.java
+++ src/main/java/io/github/conphucious/ebr/controller/ItemController.java
@@ -21,13 +21,16 @@ public class ItemController {
             ItemStatus itemStatus = getStatus(document);
 
             if (itemStatus == ItemStatus.ENDED) {
+                System.out.println("ENDED");
+                // item type stays null
                 return null;
             } else if (itemStatus == ItemStatus.IN_PROGRESS) {
                 ItemType itemType = getType(document);
             }
 
             ItemType itemType = getType(document);
-            System.out.println(itemType);
+            String itemPrice = getPrice(document, itemType);
+            System.out.println(itemPrice);
 
 
 
@@ -49,14 +52,27 @@ public class ItemController {
         // Check if this is a BID
         Element priceElement = document.getElementById(Identifiers.ITEM_TYPE_BID_IDENTIFIER);
         if (priceElement == null) {
-            // Check if this is a BUY or BEST OFFER (+ BUY)
-            Elements bestOfferElement = document.select("div[class=\"lbl\"]");
-            itemType = bestOfferElement.text().isEmpty() ? ItemType.BUY : ItemType.BEST_OFFER;
+            // Check if this is a BEST_OFFER or BUY
+            Element bestOfferElement = document.getElementById(Identifiers.ITEM_TYPE_BEST_OFFER_IDENTIFIER);
+            itemType = bestOfferElement == null ? ItemType.BUY : ItemType.BEST_OFFER;
         } else {
             itemType = ItemType.BID;
         }
 
         return itemType;
+    }
+
+    private String getPrice(Document document, ItemType itemType) {
+        System.out.println(itemType);
+        Element priceElement = null;
+        if (itemType == ItemType.BEST_OFFER) {
+            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BEST_OFFER_IDENTIFIER);
+        } else if (itemType == ItemType.BUY) {
+            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BUY_IDENTIFIER);
+        } else if (itemType == ItemType.BID) {
+            priceElement = document.getElementById(Identifiers.ITEM_PRICE_BID_IDENTIFIER);
+        }
+        return priceElement.text();
     }
 
 
--- src/main/java/io/github/conphucious/ebr/model/Identifiers.java
+++ src/main/java/io/github/conphucious/ebr/model/Identifiers.java
@@ -4,6 +4,11 @@ public class Identifiers {
 
     public static final String ITEM_STATUS_IDENTIFIER = "w1-5-_msg";
 
+    public static final String ITEM_PRICE_BID_IDENTIFIER = "prcIsum_bidPrice";
+    public static final String ITEM_PRICE_BUY_IDENTIFIER = "prcIsum";
+    public static final String ITEM_PRICE_BEST_OFFER_IDENTIFIER = "mm-saleDscPrc";
+
     public static final String ITEM_TYPE_BID_IDENTIFIER = "prcIsum_bidPrice";
+    public static final String ITEM_TYPE_BEST_OFFER_IDENTIFIER = "boBtn_btn";
 
 }
